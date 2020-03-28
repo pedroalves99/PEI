8c09ab6e1ba327e90b155c12fd12a78132f1290c
rubenmenino <ruben.menino@outlook.com>
2020-03-28 20:26:35 +0000

    guardar video

--- Processamento/code.py
+++ Processamento/code.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture('HENRIQUE03.wmv') #começa a captura de video(por o nome do video como argumento, e coloca-lo no mesmo diretorio)
+cap = cv2.VideoCapture('MENINO01.wmv') #começa a captura de video(por o nome do video como argumento, e coloca-lo no mesmo diretorio)
 
 if not cap.isOpened():
     print("Erro")
@@ -16,9 +16,13 @@ lk_params = dict(winSize = (15, 15),
 
 point_selected = False
 flag = 1
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # width do frame
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # height do frame
+fps = 30.0
+fourcc = cv2.VideoWriter_fourcc(*'XVID')    # define saida
 
-#Mouse Function
-def addPoint(x, y):#À medida que são selecionados pontos estes são adicionados ao array
+# Mouse Function
+def addPoint(x, y):# À medida que são selecionados pontos estes são adicionados ao array
     global old_points, origin_points
     a_point = np.array([[x, y]], dtype=np.float32) # formata as coordenadas x,y(float32)
     old_points = np.append(a_point, old_points, axis=0)#faz append das coordenadas ao array
@@ -36,22 +40,32 @@ def select_point(event, x, y, flags, params): #Chamada quando se clica no video,
         cv2.circle(p_frame, (x, y), 5, (0, 255, 0), -1) #sempre que é clicado na imagem, faz um circulo a volta das coord
         #print(old_points)
 
+# save_video('video.avi', 20, mirror=True)   depois vejo...
+def save_video(outPath, fps, mirror=False):
+    out = cv2.VideoWriter('abc.avi', fourcc, fps, (int(width), int(height)))
+    while(cap.isOpened()):
+        if(check==True):
+            out.write(frame)
+
 cv2.namedWindow("Frame")
 cv2.setMouseCallback("Frame", select_point) #quando se carrega no rato ativa a funçao select_point
 
+
+out = cv2.VideoWriter('video.avi', fourcc, fps, (int(width), int(height))) # e cria o ojeto VideoWriter para o primeiro while
 while True:# Este while serve para a primeira imagem ficar parada até o utilizador pressionar ('p') -> util para o utilizador selecionar os pnts
     cv2.imshow('Frame', p_frame)
 
     if cv2.waitKey(27) & 0xFF == ord('p'):
             break
 
+out = cv2.VideoWriter('video.avi', fourcc, fps, (int(width), int(height))) # # e cria o objeto VideoWriter para o segundo while
 while True:
 
     check ,frame = cap.read() #le frame a frame
 
     if not check:# entra neste if quando acaba os frames do video, abre-se outra captura para manter em loop
 
-        cap1 = cv2.VideoCapture('HENRIQUE03.wmv')#abrir nova captura
+        cap1 = cv2.VideoCapture('MENINO01.wmv')#abrir nova captura
 
         if not cap1.isOpened():
             print("Erro")
@@ -80,9 +94,9 @@ while True:
         for x,y in new_points:#por todos s novos pontos desenha um circulo verde à volta
             cv2.circle(frame, (x,y), 5, (0,255,0), 2)
 
-
+    out.write(frame)    # grava o video depois dos pontos selecionados/ começa a gravar depois de premida a letra 'p' e grava continuadamente até se premida a tecla ESC
     cv2.imshow("Frame", frame)
-    key = cv2.waitKey(100)
+    key = cv2.waitKey(20)
 
     if key == 27: #ESC
         break
